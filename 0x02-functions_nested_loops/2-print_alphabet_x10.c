47e1e8fefc2dc8ca6c04b78fb2325973c003ca80
SOGs12 <mosesoyewumi05@gmail.com>
2023-06-21 03:27:41 -0700

    2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -5,14 +5,14 @@
  */
 void print_alphabet_x10(void)
 {
-    int i, j;
+	int i, j;
 
-    for (i = 0; i < 10; i++)
-    {
-	for (j = 0; j < 26; j++)
+	for (i = 0; i < 10; i++)
 	{
-	       	_putchar('a' + j);
+		for (j = 0; j < 26; j++)
+		{
+			_putchar('a' + j);
+		}
+		_putchar('\n');
 	}
-	_putchar('\n');
-    }
 }

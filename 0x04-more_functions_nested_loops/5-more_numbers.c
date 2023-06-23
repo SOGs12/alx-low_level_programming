f1f64daf25eaab0623fd5494d59c7535f4b77c21
SOGs12 <mosesoyewumi05@gmail.com>
2023-06-23 12:56:50 -0700

    5-more_numbers.c

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * more_numbers - Print numbers from 0-14 10 times
+ * Return: void
+ */
+void more_numbers(void)
+{
+
+int i, j;
+	for (i = 1; i <= 10; i++)
+	{
+		for (j = 0; j <= 14; j++)
+		{
+			if(j >= 10)
+			putchar('1');
+			putchar(j % 10 + '0');
+		}
+			putchar('\n');
+		}
+}

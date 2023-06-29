eb92441f421ec9db906899dd034e65d8be83f6d8
SOGs12 <mosesoyewumi05@gmail.com>
2023-06-29 07:01:04 -0700

    3-strcmp.c

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -1,10 +1,12 @@
 #include "main.h"
 
 /**
- * _strcmp - Compare sting value
- * @s1: input value
- * @s2: input value
- * Return: s1[i] = s2[i]
+ * _strcmp - Compare two strings.
+ * @s1: The first string.
+ * @s2: The second string.
+ *
+ * Return: 0 if the strings are equal, a negative value if s1 is less than s2,
+ *         or a positive value if s1 is greater than s2.
  */
 int _strcmp(char *s1, char *s2)
 {
@@ -15,7 +17,7 @@ int _strcmp(char *s1, char *s2)
 	{
 		if (s1[i] != s2[i])
 		{
-			return (s1[i] = s2[i]);
+			return (s1[i] - s2[i]);
 		}
 		i++;
 	}

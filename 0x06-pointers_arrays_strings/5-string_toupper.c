7b3c8cbd883440b8e59f659cbe8a520934dc314d
SOGs12 <mosesoyewumi05@gmail.com>
2023-06-30 14:39:41 -0700

    5-string_toupper.c

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -8,13 +8,11 @@
  */
 char *string_toupper(char *str)
 {
-	int index;
-
-	index = 0;
+	int index = 0;
 
 	while (str[index] != '\0')
 	{
-		if (str[index] >= 'a' && str[index] <= 'Z')
+		if (str[index] >= 'a' && str[index] <= 'z')
 			str[index] = str[index] - 32;
 		index++;
 	}

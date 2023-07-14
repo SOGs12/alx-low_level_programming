9cb5c908d0dca5daf1f7f384ad872be6fbc8451c
SOGs12 <mosesoyewumi05@gmail.com>
2023-07-14 05:02:26 -0700

    101-mul.c

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,10 +2,10 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-define ERR_MSG "Error"
+#define ERR_MSG "Error"
 
 /**
- * is_digit - checks if a strinh contains a non-digit char
+ * is_digit - checks if a string contains a non-digit char
  * @s: string to be evaluated
  *
  * Return: 0 if a non-digit is found, 1 otherwise
@@ -62,7 +62,7 @@ int main(int argc, char *argv[])
 	int len1, len2, len, i, carry, digit1, digit2, *result, a = 0;
 
 	s1 = argv[1], s2 = argv[2];
-	if (argv != 3 || !is_digit(s1) || !is_digit(s2))
+	if (argc != 3 || !is_digit(argv[1]) || !is_digit(argv[2]))
 		errors();
 	len1 = _strlen(s1);
 	len2 = _strlen(s2);
@@ -83,6 +83,9 @@ int main(int argc, char *argv[])
 			result[len1 + len2 + 1] = carry % 10;
 			carry /= 10;
 		}
+		if (carry > 0)
+			result[len1 + len2 + 1] += carry;
+	}
 	for (i = 0; i < len - 1; i++)
 	{
 		if (result[i])
